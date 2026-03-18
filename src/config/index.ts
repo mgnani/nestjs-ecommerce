670bccc3ede1323b885d860e104d94c8b16ca5eb
mgnani <nani07@hotmail.com.ar>
2026-03-18 15:40:11 -0300

    start project

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,10 +8,10 @@ config({ path: envFilePath });
 
 export const configuration = () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
-  baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+  baseUrl: process.env.BASE_URL || 'http://127.0.0.1:3000',
   database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    host: process.env.DATABASE_HOST || '127.0.0.1',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5433,
     name: process.env.DATABASE_NAME || 'ecommercedb',
     user: process.env.DATABASE_USER || 'hassan',
     password: process.env.DATABASE_PASSWORD || 'password',
